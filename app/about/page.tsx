676ef5e53f7d6ba01e9d3961e93be2eb9f618537
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-11-30 22:35:08 +0100

    refactor: Reorder imports for consistency in AboutPage, RootPage, and RegisterPage components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
 import { defaultLocale } from '@/i18n'
+import { redirect } from 'next/navigation'
 
 export default function AboutPage() {
   redirect(`/${defaultLocale}/about`)
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
 import { defaultLocale } from '@/i18n'
+import { redirect } from 'next/navigation'
 
 export default function RootPage() {
   redirect(`/${defaultLocale}`)
